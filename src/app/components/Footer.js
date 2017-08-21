08b0c768a9b1252277461e4fe73f83290664b123
Markus Tuominen <markus.tuominen91@gmail.com>
2017-08-21 16:12:08 +0300

    fixes

---
+++ src/app/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Footer = (props) => {
+    return (
+        <nav className="navbar navbar-inverse">
+            <div id="navText" className="container">
+                <p>Laske kuinka paljon rahaa menee bensaan</p>
+            </div>
+        </nav>
+    );
+};
--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -7,6 +7,7 @@ export class Home extends React.Component {
         var km = parseFloat(document.getElementById("km"));
         var fuel = parseFloat(document.getElementById("fuel"));
         var price = parseFloat(document.getElementById("price"));
+        var howMuch = document.getElementById("howMuch");
         var total = 0;
         var all = 0;
     }    
@@ -20,23 +21,27 @@ export class Home extends React.Component {
         var total = ((fuel.value / 100) * km.value) * price.value;
         var all = parseFloat(total).toFixed(2);      
         console.log("Polttoaineen hinta: " + all);
+
+        howMuch.innerHTML = "Polttoaineen kustannukset: " + all + " €";
     }
     
-    onHandleChange(event) {
-        this.setState({
-            homeLink: event.target.value
-        });
+    onEmpty() {
+        km.value = "";
+        fuel.value = "";
+        price.value = "";
     }
 
     render() {       
         return (
-            <div>               
-                <hr/>
+            <div className="col-xs-10 col-xs-offset-1">               
+                <br/>
                 <input id="km" type="number" placeholder="Ajetut kilometrit"/>
                 <input id="fuel" type="number" placeholder="Keskikulutus / 100 km"/>
                 <input id="price" type="number" placeholder="Polttoaineen litrahinta"/>
-                <p id="howMuch"></p>
-                <hr/>  
+                <div id="total-container">
+                    <h3 id="howMuch"></h3>
+                </div>
+                <br/>
                 <button onClick={this.onCalculate} className="btn btn-success btn-md">Laske kustannukset</button>
                 <button onClick={this.onEmpty} className="btn btn-danger btn-md">Tyhjennä</button>                    
             </div>         
