c2149919ee78c06c92e98559d3e51291d8669853
Markus Tuominen <markus.tuominen91@gmail.com>
2017-08-21 12:12:02 +0300

    cleaning up a bit

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,9 +1,8 @@
 import React from "react";
-import PropTypes from 'prop-types'; // PropTypes toimi ennen perus-React paketin kanssa, mutta nykyään pitää importata erikseen PropTypes
 
 export class Home extends React.Component {   
 
-    constructor(props) {
+    constructor() {
         super();
         var km = parseFloat(document.getElementById("km"));
         var fuel = parseFloat(document.getElementById("fuel"));
@@ -36,20 +35,11 @@ export class Home extends React.Component {
                 <input id="km" type="number" placeholder="Ajetut kilometrit"/>
                 <input id="fuel" type="number" placeholder="Keskikulutus / 100 km"/>
                 <input id="price" type="number" placeholder="Polttoaineen litrahinta"/>
+                <p id="howMuch"></p>
                 <hr/>  
                 <button onClick={this.onCalculate} className="btn btn-success btn-md">Laske kustannukset</button>
-                <button onClick={this.onEmpty} className="btn btn-danger btn-md">Tyhjennä</button>
-                <div>
-                    <p>Tulos</p>
-                </div>                    
+                <button onClick={this.onEmpty} className="btn btn-danger btn-md">Tyhjennä</button>                    
             </div>         
         );
     }
 }
-
-// Home.propTypes = { // määrittää mitä tyyppiä minkäkin arvon tulee olla. Jollei esim nimi ole string = error
-//     name: PropTypes.string,
-//     age: PropTypes.number,
-//     greet: PropTypes.func,
-//     initialLinkName: PropTypes.string
-// };
